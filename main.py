f50a21b5efa14e6b2598ee4c9915bf0f6e52f82a
nikkirche <nikkirche@hotmail.com>
2023-04-23 15:39:24 +0300

    added skeleton

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 import docker
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs", redoc_url=None)
 client = docker.from_env()
 
 
-@app.get("/api/phoronix-perf")
-async def create_phoronix_perf_process():
-    client.containers.run("main", "phoronix-test-suite batch-run  perf-bench", detach=True)
+@app.post("/api/phoronix-perf")
+async def create_phoronix_perf_process(option:str):
+    job ="phoronix-test-suite batch-run  perf-bench"
+    if(option=="start"):
+        client.containers.run("main",job , detach=True)
+    elif(option=="stop"):
+        pass
+    else:
+        pass
+@app.get("api/phoronix-perf")
+async def status_phoronix_perf_process():
+    pass
+@app.get("/api/phoronix-perf/result")
+async def result_phoronix_perf_process():
+    pass
